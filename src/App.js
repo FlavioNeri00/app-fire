9daeb6853a5205f4bb017fedd3c76e3f132f9c2a
Flavio Neri <136917681+FlavioNeri00@users.noreply.github.com>
2023-10-20 10:42:52 +0200

    firebase running

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { collection, getDocs } from "firebase/firestore";
+import { db } from ".";
+
+class App extends Component {
+  state = {
+    notes: [],
+  };
+
+  async refreshNotres() {
+    let notesList = [];
+
+    const notesCol = collection(db, "Menu", "Tavola calda", "Pizza");
+    const notesSnapshot = await getDocs(notesCol);
+    console.log(notesSnapshot);
+
+    // notesSnapshot.forEach((doc) => {
+    //   let note = doc.data();
+    //   note.id = doc.id;
+    //   notesList.push(note);
+    // });
+
+    notesList.push(
+      ...notesSnapshot.docs.map((elem) => ({ ...elem.data(), id: elem.id }))
+    );
+
+    console.log(notesList);
+    // this.setState({ notes: notesList });
+    // this.setState({ notes: notesList });
+    console.log(this.setState({ notes: notesList }));
+  }
+  componentDidMount() {
+    this.refreshNotres();
+  }
+
+  render() {
+    const { notes } = this.state;
+
+    return (
+      <div className="App">
+        <h2>Todo List</h2>
+        {notes.map((el, index) => (
+          <ul key={index}>
+            <li>* {el.id}</li>
+          </ul>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCy_9f-fLJZqdKg_LYCVz-e3zYAfTtY9K8",
+  authDomain: "scaro-1.firebaseapp.com",
+  projectId: "scaro-1",
+  storageBucket: "scaro-1.appspot.com",
+  messagingSenderId: "305858532206",
+  appId: "1:305858532206:web:ba746c1bf370240180677f",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+console.log(db);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
